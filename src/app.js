a7133b0efd676e64c3588e6d6b3e19d0f669d566
Jayasuriya-R <suryaabi3801@gmail.com>
2025-09-30 15:53:16 +0530

    routing based on request

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("server is satrted successfully")
+app.listen(3000, () => {
+  console.log("server is satrted successfully");
 });
 
-app.use("/1",(req,res)=>{
-    res.send("hello from server")
-})
+app.use("/1/2", (req, res) => {
+  res.send("hello from server 2..");
+});
 
-app.use("/guest",(req,res)=>{
-    res.send("hello from guest")
-})
+app.use("/1", (req, res) => {
+  res.send("hello from server 1..");
+});
+
+app.use("/guest", (req, res) => {
+  res.send("hello from guest");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("hello from server");
+// });
