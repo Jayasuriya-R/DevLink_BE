f0b67ff0e8c12467259884e532ed6154d0c31a9a
Jayasuriya-R <suryaabi3801@gmail.com>
2025-10-09 18:03:46 +0530

    patch,delete and get end points

--- src/app.js
+++ src/app.js
@@ -9,15 +9,52 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-  console.log(req.body)
-  
   const user = new User(req.body);
   try {
     await user.save();
     res.send("user added successfully");
   } catch (err) {
     res.status(400).send("error occured", err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const data = await User.find({ emailId: req.body.emailId });
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("something went wrong", err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await User.find({});
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("something went wrong", err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.id);
+    res.send("deleted successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong", err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const data = await User.findByIdAndUpdate(req.body._id, {
+      firstName: req.body.firstName
+    },{ new: true } );
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("something went wrong", err.message);
   }
 });
 
