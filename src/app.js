2393b28dce91ca5362cb97dbd7718ddc8aaf219a
Jayasuriya-R <suryaabi3801@gmail.com>
2025-11-21 13:28:29 +0530

    updated the port

--- src/app.js
+++ src/app.js
@@ -67,6 +67,7 @@ initializeSocket(server)
 connectDB()
   .then(() => {
     console.log("DB connection successful");
-    server.listen(3000, () => console.log("Server started on port 3000"));
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => console.log("Server started on port 3000"));
   })
   .catch(() => console.log("Connection failed"));
